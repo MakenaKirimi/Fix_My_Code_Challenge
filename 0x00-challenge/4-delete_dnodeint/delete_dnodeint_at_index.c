136571f400bd41f8730dbbf9c18c4e56f58f82d9
MakenaKirimi <makenafaith12@gmail.com>
2022-08-22 04:08:20 +0300

    fix my code

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp;
 	unsigned int p;
 
-	if ((head == NULL) || (*head == NULL))
+	if (*head == NULL)
 	{
 		return (-1);
 	}
@@ -43,10 +43,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->next = (*head)->next;
-		free(*head);
 		if ((*head)->next)
+		{
+			(*head)->prev->next = (*head)->next;
 			(*head)->next->prev = (*head)->prev;
+		}
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
